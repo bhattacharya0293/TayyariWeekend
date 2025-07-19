767008fb377940f45d76fbfb090632e21f128dd4
bhattacharya0293 <bhattacharya0293@gmail.com>
2025-07-19 02:04:34 -0500

    added Conditions

---
+++ src/Conditionals/RelationalOperators.java
@@ -0,0 +1,23 @@
+package Conditionals;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        System.out.println(7<90);
+        System.out.println(5>2);
+        System.out.println(7>56);
+        System.out.println(7>=7);
+        System.out.println(7>=6);
+        System.out.println(3 != 8);
+        System.out.println(8+3 > 10);
+        System.out.println(8+3 == 10-9);
+
+        int a = 7, b = 9;
+        System.out.println(a==b);
+        System.out.println(a=b); // a me b ki value aayi and then a print hua
+        System.out.println(a);
+
+        boolean flag = a<b;
+        System.out.println(flag);
+
+    }
+}
